dde3d8d0ebcf25ceed3f0cdbe7ae5a304c3f0e34
espenb08 <espenb08.kodehode@gmail.com>
2023-11-28 12:50:20 +0100

    category buttons

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,12 @@
 import quizObject from "./quizObject.json" assert { type: "json" };
 
 console.log(quizObject);
+const sideBar = document.querySelector(".sidebar")
+const buttons = Object.keys(quizObject);
+for (let button of buttons) {
+  const btn = document.createElement("button");
+  const btnText = document.createTextNode(button);
+  btn.appendChild(btnText);
+  sideBar.appendChild(btn);
+}
+console.log(Object.keys(quizObject));
