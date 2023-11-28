93f2f60f26dde572a34ad0e36db7dcf08097ca61
espenb08 <espenb08.kodehode@gmail.com>
2023-11-28 13:14:00 +0100

    display and hide  action on hamburger icon

--- JS/index.js
+++ JS/index.js
@@ -11,6 +11,7 @@ for (let button of buttons) {
 }
 console.log(Object.keys(quizObject));
 
+let menuOpen = false
 const hamburgerMenu = document.createElement("img");
 hamburgerMenu.setAttribute("src", "img/hamburger-button.svg");
 hamburgerMenu.setAttribute("width", "48");
@@ -18,5 +19,12 @@ hamburgerMenu.setAttribute("height", "48");
 hamburgerMenu.setAttribute("alt", "test");
 document.body.appendChild(hamburgerMenu);
 hamburgerMenu.addEventListener("click", () => {
+    if (menuOpen === false){
+    sideBar.style.display = "block";
+    menuOpen = true
+    }else{
+        sideBar.style.display = "none"
+        menuOpen = false
+    }
   console.log("hjelpameg");
 });
