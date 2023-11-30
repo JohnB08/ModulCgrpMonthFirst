c24803c7a98fb035d8bfd617be18c027b86ac94d
Alexsarkodehode <alexsar.kodehode@gmail.com>
2023-11-30 12:41:59 +0100

    function selectAnswer

--- JS/index.js
+++ JS/index.js
@@ -80,3 +80,19 @@ function resetState(){
         answerButtons.removeChild(answerButtons.firstChild)
     }
 }
+
+function selectAnswer(){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true"
+    if(isCorrect){
+        selectedBtn.classList.add("correct")
+    } else {selectedBtn.classList.add("incorrect");}
+    
+    Array.from(answerButtons.children).forEach(button => {
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct")
+        }
+        button.disabled = true 
+    })
+    nextButton.style.display = "block"
+}
