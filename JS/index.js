c2674ea9fb8ca0954ccca617b0ba9ea97d32448e
Alexsarkodehode <alexsar.kodehode@gmail.com>
2023-11-30 12:40:15 +0100

    function showQuestion

--- JS/index.js
+++ JS/index.js
@@ -53,3 +53,23 @@ function startQuiz(){
     let score = 0
     nextButton.innerHTML = "Next"
     showQuestion();
+}
+
+function showQuestion(){
+    resetState()
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button")
+        button.innerHTML = answer.text
+        button.classList.add("btn")
+        answerButtons.appendChild(button)
+        if(answer.correct){
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener("click", selectAnswer)
+    }) 
+
+}
