c32b5342888c240a6c31fe0a27aa71b72e0c970e
JohnB08 <142892183+JohnB08@users.noreply.github.com>
2023-12-07 12:25:49 +0100

    test

--- JS/questionFetcher.js
+++ JS/questionFetcher.js
@@ -100,7 +100,7 @@ const writeObject = async () => {
 };
 
 /* Scheduler som oppdaterer quizObject hver midnatt. */
-schedule.scheduleJob("0 5 * * *", async () => {
+schedule.scheduleJob("20 12 * * *", async () => {
   console.log("starting script");
   await writeObject();
   console.log("adding to git commit");
